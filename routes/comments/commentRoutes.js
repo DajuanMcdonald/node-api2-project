b50fd142d9d876ece5f794dd59911429b39e55a5
Dajuan Mcdonald <flopwiki@gmail.com>
2020-04-20 18:37:58 -0400

    modified test file: commentRoutes.js

--- routes/comments/commentRoutes.js
+++ routes/comments/commentRoutes.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../../data/db');
 
 
 
 router.get('/:id/comments', (req, res) => {
+    const id = req.params.id;
+    if(!id) {
+        res.status(404).json({message: "The post with the specified ID does not exist."})
+    } else {
+        db.findCommentById(id)
+        .then( comment => {
+            res.status(200).json(comment)
+        }).catch(err => res.status(500).json({message: "The comments information could not be retrieved."}))
+    }
     res.status(200).send('<p>Server-Side Routing with Express</p>');
 });
 
