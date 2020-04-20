f3b71ae3cf20a4c72185b398a50436dbe612dd80
Dajuan Mcdonald <flopwiki@gmail.com>
2020-04-20 13:03:35 -0400

    added GET request for findById()

--- routes/posts/postRoutes.js
+++ routes/posts/postRoutes.js
@@ -4,10 +4,26 @@ const db = require('../../data/db');
 
 router.use(express.json());
 
+
+//When the client makes a `GET` request to `/api/posts`:
 router.get('/', (req, res) => {
     db.find()
     .then(posts => {
         res.status(200).json(posts)
+    })
+    .catch(err => {
+        res.status(500).json({error: "The posts information could not be retrieved."})
+    })
+})
+
+//When the client makes a `GET` request to `/api/posts/:id`:
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    !id ? res.status(404).json({message: "The post with the specified ID does not exist."}) :
+    
+    db.findById(id)
+    .then(post => {
+        res.status(200).json(post)
     })
     .catch(err => {
         res.status(500).json({error: "The posts information could not be retrieved."})
