02138569b3e6403cd6b6e57c59078c75c38274ee
Dajuan Mcdonald <flopwiki@gmail.com>
2020-04-20 15:18:47 -0400

    fixed error on post/:id

--- routes/posts/postRoutes.js
+++ routes/posts/postRoutes.js
@@ -46,7 +46,7 @@ router.get('/', (req, res) => {
 * 
 */
 router.get('/:id', (req, res) => {
-    const {id} = req.params.id;
+    const id = req.params.id;
     !id ? res.status(404).json({message: "The post with the specified ID does not exist."}) :
     
     db.findById(id)
